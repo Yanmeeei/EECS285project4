09fb7f4f423ed50f9d41572886465d3e4307d40a
Wang Yanmei <wyanmei@umich.edu>
2021-11-15 14:07:13 -0500

    init commit

---
+++ app/src/main/java/eecs285/proj4/dawnsqrl_wyanmei/budgettracker/MainActivity.java
@@ -0,0 +1,14 @@
+package eecs285.proj4.dawnsqrl_wyanmei.budgettracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
