bc79d0a4340482af2de1a184495b287808b5294a
Wang Yanmei <wyanmei@umich.edu>
2021-11-23 14:58:14 -0500

    updated error message

--- app/src/main/java/eecs285/proj4/dawnsqrl_wyanmei/budgettracker/AddTransactionDialogFragment.java
+++ app/src/main/java/eecs285/proj4/dawnsqrl_wyanmei/budgettracker/AddTransactionDialogFragment.java
@@ -5,7 +5,9 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -43,6 +45,81 @@ public class AddTransactionDialogFragment extends DialogFragment {
         LayoutInflater inflater = LayoutInflater.from(getActivity());
         builder.setTitle(R.string.title_dialog_add);
         View view = inflater.inflate(R.layout.dialog_add_transaction, null);
+
+        TextInputLayout titleInputLayout = view.findViewById(R.id.titleInputLayout);
+        TextInputLayout categoryInputLayout = view.findViewById(R.id.categoryInputLayout);
+        TextInputLayout costInputLayout = view.findViewById(R.id.costInputLayout);
+
+        EditText titleText = view.findViewById(R.id.nameInput);
+        EditText categoryText = view.findViewById(R.id.categoryInput);
+        EditText costText = view.findViewById(R.id.costInput);
+
+        titleText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.length() > 0) {
+                    titleInputLayout.setError(null);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s.length() == 0) {
+                    titleInputLayout.setError("required input");
+                    titleText.requestFocus();
+                } else {
+                    titleText.setError(null);
+                }
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+        });
+
+        categoryText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.length() > 0) {
+                    categoryInputLayout.setError(null);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s.length() == 0) {
+                    categoryInputLayout.setError("required input");
+                    categoryText.requestFocus();
+                } else {
+                    categoryText.setError(null);
+                }
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+        });
+
+        costText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.length() > 0) {
+                    costInputLayout.setError(null);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s.length() == 0) {
+                    costInputLayout.setError("required input");
+                    costText.requestFocus();
+                } else {
+                    costText.setError(null);
+                }
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+        });
+
 
         builder.setView(view).setPositiveButton(R.string.button_add,
                 (dialogInterface, id) -> {
@@ -90,20 +167,26 @@ public class AddTransactionDialogFragment extends DialogFragment {
         if (TextUtils.isEmpty(titleText.getText().toString())
             || TextUtils.isEmpty(categoryText.getText().toString())
             || TextUtils.isEmpty(costText.getText().toString())) {
-
-            if (TextUtils.isEmpty(titleText.getText().toString())) {
-                titleInputLayout.setError("required input");
-                titleText.requestFocus();
-            }
+//
+//            if (TextUtils.isEmpty(titleText.getText().toString())) {
+//                titleInputLayout.setError("required input");
+//                titleText.requestFocus();
+//            } else {
+//                titleInputLayout.setError(null);
+//            }
 
             if (TextUtils.isEmpty(categoryText.getText().toString())) {
                 categoryInputLayout.setError("required input");
                 categoryText.requestFocus();
+            } else {
+                categoryInputLayout.setError(null);
             }
 
             if (TextUtils.isEmpty(costText.getText().toString())) {
                 costInputLayout.setError("required input");
                 costText.requestFocus();
+            } else {
+                costInputLayout.setError(null);
             }
 
             return false;
